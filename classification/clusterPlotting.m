2903d12fdb305fa955951c34314ccbe89eebac54
RXA254 <rana@caltech.edu>
2017-05-12 13:55:08 -0700

    * modified indexing to handle more seismometers * does channel name parsing to label the plot and the title more cleanly

--- classification/clusterPlotting.m
+++ classification/clusterPlotting.m
@@ -5,8 +5,9 @@ k = 10;                  % number of clusters
 total_time = 30 * 24;   % number of hours
 
 %data = load(strcat(working_dir,'/data_array.mat'));
-blrms = load(['Data/' ifo '_SeismicBLRMS_March.mat']);
-zidx = 1:18;
+blrms = load(['Data/' ifo '_SeismicBLRMS.mat']);
+[a,b] = size(blrms.data);
+zidx = 1:a;
 data = blrms.data(zidx,:).';
 
 % for some reason this array has a 13th column of zeros - need to fix in
@@ -15,7 +16,7 @@ data = blrms.data(zidx,:).';
 
 % k cluster kmeans
 tic
-[idx, C] = kmeans(data, k);
+[idx, C] = kmeans(log10(data+1), k);
 display([num2str(toc) ' s to do the clustering.'])
 
 %% generate time column for plotting 
@@ -31,38 +32,51 @@ channels = blrms.chans(zidx,:);
 % plot clusters for each channel and save
 c = distinguishable_colors(k);
 
+n_bands = 6;   % number of frequency bands per channel
 
-
-for sensor = 2
+for sensor = 1
     %close(200 + sensor)
     clf
     figure(200 + sensor)
-    [ha,pos] = tight_subplot(6, 1, 0, [0.1 0.031], [0.06 0.03]);
-    for v = 1:6
+    [ha,pos] = tight_subplot(6, 1, 0, [0.25 0.031], [0.06 0.03]);
+    for v = 1:n_bands     % go through all the frequency bands for this sensor DoF
       axes(ha(v));
+      chan = n_bands*(sensor-1) + v;
       for clust = 1:k
         n = find(idx == clust);
-        chan = 6*(sensor-1) + v;
         scatter(tt(n)/24, data(n, chan), 2, c(clust,:))
         hold on
       end
-    set(gca,'YScale','log')
-    set(gca,'XTick',[])
-    pp = find(data(:,chan));   % find non-zero elements
-    bott = min(data(pp,chan))   % find min of all non-zero elements
-    bott = max(bott, 3);      % bottom of scale can't be less than ...
-    ylim([0.9*bott 110*bott])
-    %axis tight
-    legg = legend(channels(chan,:), 'Location', 'NorthWest');
-    set(legg,'Interpreter','none');
-    set(legg,'FontSize',10);
-    g = floor(log10(bott));
-    set(gca, 'YTick', 10.^([g g+1 g+2]))
-    %title(strcat('Cluster Assignments for Channel', ' ', channels(channel, 1)))
-    %savefig(strcat(working_dir,'/clusterPlots/channel', num2str(channel - 1), 'clusters.fig'))
+      set(gca,'YScale','log')
+      set(gca,'XTick',[])
+      
+      pp = find(data(:,chan));   % find non-zero elements
+      bott = min(data(pp,chan));   % find min of all non-zero elements
+      bott = max(bott, 3);      % bottom of scale can't be less than ...
+      ylim([0.9*bott 110*bott])
+      %axis tight
+      
+      chan_str = channels(chan,:);
+      rr = strfind(chan_str, 'BLRMS');
+      rs = strfind(chan_str, '.mean');
+      if v==1
+          title_str = chan_str(1:(rr-2));
+          title(title_str,...
+                'Interpreter', 'none')
+      end
+      
+      legg = legend(chan_str((rr+6):(rs-1)),...
+                    'Location', 'NorthWest');
+      set(legg, 'Interpreter', 'none');
+      set(legg, 'FontSize'   , 10);
+      
+      g = floor(log10(bott));
+      set(gca, 'YTick', 10.^([g g+1 g+2]));
+      
     end
-    set(gca,'Xtick',[0:7:42])
-    xlabel('Time (days)')
+    set(gca,'Xtick',[0:1:32])
+    xlabel('Time [days]',...
+           'Interpreter','Latex')
     %ylabel('RMS Velocity (micrometers/s)')
     %legg = legend(channels, 'Location', 'SouthWest');
     %set(legg,'Interpreter','none');
@@ -71,7 +85,9 @@ end
 tic
 pause(1.5)
 set(gcf,'Position', [0 0 1900 1200])
-hh = text(-1.3, 5000000, 'RMS Velocity [um/s]','Interpreter','Latex','rotation',90);
+hh = text(-1.3, 5000000, 'RMS Velocity [$\mu$m/s]',...
+          'Interpreter','Latex',...
+          'rotation',90);
 %% pretty print
 
 

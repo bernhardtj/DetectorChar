f93b5d7756829c1ed293cb38e445a410fa6acc8c
Roxana Popescu <roxana.popescu@ldas-pcdev1.ligo.caltech.edu>
2017-07-27 13:23:02 -0700

    updated test_clusters.py

--- classification/test_clusters.py
+++ classification/test_clusters.py
@@ -124,9 +124,9 @@ fig.savefig('Figures/EQs_all_indicated')
 kmeans = KMeans(n_clusters=cl, random_state=12).fit(timetuples3) #kmeans clustering of earthquake channels
 db = DBSCAN(eps=eps,min_samples=min_samples).fit(timetuples3) #dbscan clustering of earthquake channels
 #ag = AgglomerativeClustering(n_clusters = cl2, linkage='complete').fit(timetuples3) #agglomerative clustering of earthquake channels
+#birch = Birch(n_clusters=cl3).fit(timetuples3)
 
 #other clustering algorithms
-birch = Birch(n_clusters=cl3).fit(timetuples3)
 #af = AffinityPropagation(preference=preference).fit(timetuples)
 #bandwidth = estimate_bandwidth(timetuples, quantile=0.2, n_samples=100)
 #ms = MeanShift(bandwidth=bandwidth,bin_seeding=True)
@@ -161,29 +161,40 @@ for i in range(len(a)):
     agpoint = ag.labels_[val]
     agclusters = np.append(agclusters,agpoint)
     bpoint= birch.labels_[val]
-    bclusters = np.append(bclusters, bpoint)
-'''
+    bclusters = np.append(bclusters, bpoint)'''
+
 #add up number of clusters that appear next to each earthquake
-kclusters = np.array([])
-dbclusters = np.array([])
-agclusters = np.array([])
-bclusters = np.array([])
-for t in etime_march:
+kpoints = np.array([])
+dbpoints = np.array([])
+#agpoints = np.array([])
+#bpoints = np.array([])
+for t in etime_march: #for each EQ: collect indices within 5 min of EQ
     tmin = int(t-5*60)
     tmax = int(t+5*60)
     for j  in range(tmin,tmax):
         val = abs(xvals-j)
         aval = np.argmin(val)
-        kpoint = kmeans.labels_[aval]
-        kclusters = np.append(kclusters,kpoint)
-        dbpoint  = db.labels_[aval]
-        dbclusters = np.append(dbclusters,dbpoint)
-        #agpoint = ag.labels_[aval]
-        #agclusters = np.append(agclusters,agpoint)
-        bpoint = birch.labels_[aval]
-        bclusters = np.append(bclusters, bpoint)
-#histogram of clusters (commented out)                                                                                                                            
+        kpoints = np.append(kpoints, aval)
+        dbpoints  = np.append(dbpoints, aval)
+        #agpoints = np.append(agpoints, aval)
+        #bpoints = np.append(bpoints, aval)
 
+kpoints = np.unique(kpoints) #make sure there are no repeating indices
+dbpoints = np.unique(dbpoints)
+#agpoints = np.unique(agpoints)
+#bpoints = np.unique(bpoints)
+
+kclusters = np.array([])
+dbclusters = np.array([])
+#agclusters = np.array([])
+#bclusters = np.array([])
+
+for i in kpoints: kclusters = np.append(kclusters,kmeans.labels_[int(i)]) #for each index find the corresponding cluster and store them in array 
+for i in dbpoints: dbclusters = np.append(dbclusters,db.labels_[int(i)])
+#for i in agpoints: agclusters = np.append(agclusters,ag.labels_[int(i)])
+#for i in bpoints: bclusters = np.append(bclusters,birch.labels_[int(i)])
+        
+#histogram of clusters (commented out)
 '''fig = plt.figure()
 ax = fig.gca()
 ax.hist(kclusters)
@@ -194,37 +205,165 @@ ax = fig.gca()
 ax.hist(dbclusters)
 fig.savefig('/home/roxana.popescu/public_html/'+dbscan_EQ__hist.png')'''
 
-#kmeans score determined by percent of points sorted into one cluster near EQ
+#kmeans score determined by ratio of points in cluster/points near EQ to  points in cluster/all points
+print('********Results of Kmeans Clustering********')
+print('Number of points in each cluster that are near an EQ')
 print(collections.Counter(kclusters))
-k_count = collections.Counter(kclusters).most_common(1)
+print('Number of points in each cluster')
+print(collections.Counter(kmeans.labels_))
+k_count = collections.Counter(kclusters).most_common()
+ktot_count = collections.Counter(kmeans.labels_).most_common()
+k_list_cl = [x[0] for x in k_count]
 k_list = [x[1] for x in k_count]
-k_max = k_list[0]
-k_score = k_max/len(kclusters)
-print('kscore is ' + str(k_score))
+ktot_list_cl = [x[0] for x in ktot_count]
+ktot_list = [x[1] for x in ktot_count]
+k_clusters = np.array([])
+k_compare = np.array([])
+k_list2 = np.array([])
+ktot_list2 = np.array([])
+for i in range(len(k_list_cl)):
+    for j in range(len(ktot_list_cl)):
+        if k_list_cl[i] == ktot_list_cl[j]:
+            k_clusters = np.append(k_clusters,k_list_cl[i])
+            compare = k_list[i]/ktot_list[j]
+            k_compare = np.append(k_compare, compare)
+            k_list2 = np.append(k_list2, k_list[i])
+            ktot_list2 = np.append(ktot_list2, k_list[i])
+print('List with the clusters in order')
+print(k_clusters)
+print('Number of points in clusters near EQ divided by total number of points in clusters')
+print(k_compare)
+k_index = np.argmax(k_compare)
+#k_max_count = k_list2[0]
+k_max_count = k_list2[k_index]
+k_score = k_max_count/len(kclusters)
+#ktot_max_count = ktot_list[0]
+ktot_max_count = ktot_list2[k_index]
+ktot_score = ktot_max_count/len(kmeans.labels_)
+krel_score = k_score/ktot_score
+print('k_score is '+str(k_score)+', ktot_score is '+str(ktot_score)+ ', and krel_score is ' +str(krel_score))
 
 #dbscan score determined by percent of points sorted into one cluster near EQ
+print('********Results of DBSCAN Clustering********')
+print('Number of points in each cluster that are near an EQ')
 print(collections.Counter(dbclusters))
-db_count = collections.Counter(dbclusters).most_common(1)
+print('Number of points in each cluster')
+print(collections.Counter(db.labels_))
+db_count = collections.Counter(dbclusters).most_common()
+dbtot_count = collections.Counter(db.labels_).most_common()
+db_list_cl = [x[0] for x in db_count]
 db_list = [x[1] for x in db_count]
-db_max = db_list[0]
-db_score = db_max/len(dbclusters)
-print('dbscore is ' + str(db_score))
+dbtot_list_cl = [x[0] for x in dbtot_count]
+dbtot_list = [x[1] for x in dbtot_count]
+db_clusters = np.array([])
+db_compare = np.array([])
+db_list2 = np.array([])
+dbtot_list2 = np.array([])
+for i in range(len(db_list_cl)):
+    for j in range(len(dbtot_list_cl)):
+        if db_list_cl[i] == dbtot_list_cl[j]:
+            db_clusters = np.append(db_clusters,db_list_cl[i])
+            compare = db_list[i]/dbtot_list[j]
+            db_compare = np.append(db_compare, compare)
+            db_list2 = np.append(db_list2, db_list[i])
+            dbtot_list2 = np.append(dbtot_list2, db_list[i])
+print('List with the clusters in order')
+print(db_clusters)
+print('Number of points in clusters near EQ divided by total number of points in clusters')
+print(db_compare)
+db_index = np.argmax(db_compare)
+#db_max_count = db_list2[0]
+db_max_count = db_list2[db_index]
+db_score = db_max_count/len(dbclusters)
+#dbtot_max_count = dbtot_list[0]
+dbtot_max_count = dbtot_list2[db_index]
+dbtot_score = dbtot_max_count/len(db.labels_)
+dbrel_score = db_score/dbtot_score
+print('db_score is '+str(db_score)+', dbtot_score is '+str(dbtot_score)+ ', and dbel_score is ' +str(dbrel_score))
 
 #agglomerative clustering score determined by percent of points sorted into one cluster near EQ
-'''print(collections.Counter(agclusters))
-ag_count = collections.Counter(agclusters).most_common(1)
+'''print('********Results of Agglomerative Clustering********')
+print('Number of points in each cluster that are near an EQ')
+print(collections.Counter(agclusters))
+print('Number of points in each cluster')
+print(collections.Counter(ag.labels_))
+ag_count = collections.Counter(agclusters).most_common()
+agtot_count = collections.Counter(ag.labels_).most_common()
+ag_list_cl = [x[0] for x in ag_count]
 ag_list = [x[1] for x in ag_count]
-ag_max = ag_list[0]
-ag_score = ag_max/len(agclusters)
-print('agscore is ' + str(ag_score))'''
+agtot_list_cl = [x[0] for x in agtot_count]
+agtot_list = [x[1] for x in agtot_count]
+ag_clusters = np.array([])
+ag_compare = np.array([])
+ag_list2 = np.array([])
+agtot_list2 = np.array([])
+for i in range(len(ag_list_cl)):
+    for j in range(len(ag_list_cl)):
+        if ag_list_cl[i] == agtot_list_cl[j]:
+            ag_clusters = np.append(ag_clusters,ag_list_cl[i])
+            compare = ag_list[i]/agtot_list[j]
+            ag_compare = np.append(ag_compare, compare)
+            ag_list2 = np.append(ag_list2, ag_list[i])
+            agtot_list2 = np.append(agtot_list2, ag_list[i])
+print('List with the clusters in order')
+print(ag_clusters)
+print('Number of points in clusters near EQ divided by total number of points in clusters')
+print(ag_compare)
+ag_index = np.argmax(ag_compare)
+#ag_max_count = ag_list2[0]
+ag_max_count = ag_list2[ag_index]
+ag_score = ag_max_count/len(agclusters)
+#agtot_max_count = agtot_list2[0]
+agtot_max_count = agtot_list2[ag_index]
+agtot_score = agtot_max_count/len(ag.labels_)
+agrel_score = ag_score/agtot_score
+print('ag_score is '+str(ag_score)+', agtot_score is '+str(agtot_score)+ ', and agrel_score is ' +str(agrel_score))'''
 
 #birch clustering score determined by percent of points into one cluster near EQ
+'''print('********Results of Birch Clustering********')
+print('Number of points in each cluster that are near an EQ')
 print(collections.Counter(bclusters))
-b_count = collections.Counter(bclusters).most_common(1)
+print('Number of points in each cluster')
+print(collections.Counter(birch.labels_))
+b_count = collections.Counter(bclusters).most_common()
+btot_count = collections.Counter(birch.labels_).most_common()
+b_list_cl = [x[0] for x in b_count]
 b_list = [x[1] for x in b_count]
-b_max = b_list[0]
-b_score = b_max/len(bclusters)
-print('bscore is ' + str(b_score))
+btot_list_cl = [x[0] for x in btot_count]
+btot_list = [x[1] for x in btot_count]
+b_clusters = np.array([])
+b_compare = np.array([])
+b_list2 = np.array([])
+btot_list2 = np.array([])
+for i in range(len(b_list_cl)):
+    for j in range(len(btot_list_cl)):
+        if b_list_cl[i] == btot_list_cl[j]:
+            b_clusters = np.append(b_clusters,b_list_cl[i])
+            compare = b_list[i]/btot_list[j]
+            b_compare = np.append(b_compare, compare)
+            b_list2 = np.append(b_list2, b_list[i])
+            btot_list2 = np.append(btot_list2, b_list[i])
+print('List with the clusters in order')
+print(b_clusters)
+print('Number of points in clusters near EQ divided by total number of points in clusters')
+print(b_compare)
+b_index = np.argmax(b_compare)
+#b_max_count = b_list2[0]
+b_max_count = b_list2[b_index]
+b_score = b_max_count/len(bclusters)
+#btot_max_count = btot_list2[0]
+btot_max_count = btot_list2[b_index]
+btot_score = btot_max_count/len(birch.labels_)
+brel_score = b_score/btot_score
+print('b_score is '+str(b_score)+', btot_score is '+str(btot_score)+ ', and brel_score is ' +str(brel_score))'''
+
+#cluster scores using silhoutette coefficient
+#print(metrics.silhouette_score(timetuples3,kmeans.labels_))
+#print(metrics.silhouette_score(timetuples3,db.labels_))
+print(metrics.calinski_harabaz_score(timetuples3, kmeans.labels_))
+print(metrics.calinski_harabaz_score(timetuples3, db.labels_))
+#print(metrics.calinsku_harabaz_score(timetuples3, ag.labels_))
+#print(metrics.calinski_harabaz_score(timetuples3, birch.labels_))
 
 #plot graph of kmeans clustering for EQ
 xvals = np.arange(t_start,t_end,60)
@@ -276,7 +415,7 @@ fig.tight_layout()
 fig.savefig('Figures/EQ_agclustering_'+str(cl2)+'.png')'''
 
 #plot birch  clustering graph for EQ
-fig,axes  = plt.subplots(len(vdat2), figsize=(40,4*len(vdat2)))
+'''fig,axes  = plt.subplots(len(vdat2), figsize=(40,4*len(vdat2)))
 for ax, data, chan in zip(axes, vdat2, vchans2):
     ax.scatter(xvals, data,c=colors[birch.labels_],edgecolor='',
                s=3, label=r'$\mathrm{%s}$' % chan.replace('_','\_'))
@@ -289,4 +428,4 @@ for ax, data, chan in zip(axes, vdat2, vchans2):
         ax.axvline(x=etime_march[e])
 fig.tight_layout()
 #fig.savefig('/home/roxana.popescu/public_html/'+'EQ_birchclustering_'+str(cl3)+'.png')
-fig.savefig('Figures/EQ+microseism_birchclustering_'+str(cl3)+'.png')
+fig.savefig('Figures/EQ+microseism_birchclustering_'+str(cl3)+'.png')'''

0e771047d981ee7c3f06a1c40516092fcea9fbf4
Rana Adhikari <rana@caltech.edu>
2017-08-09 23:32:44 -0700

    making the plots look nicer

--- classification/kmeans_example.py
+++ classification/kmeans_example.py
@@ -9,31 +9,33 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
 
-plt.rc('text', usetex=True)
-plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
-plt.rc('axes', labelsize=20.0)
+plt.rc('text', usetex=False)
+#plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+plt.rc('axes', labelsize=22.0)
 plt.rc('axes', axisbelow=True)
 plt.rc('axes.formatter', limits=[-3,4])
-plt.rc('legend', fontsize=14.0)
-plt.rc('xtick', labelsize=16.0)
-plt.rc('ytick', labelsize=16.0)
-plt.rc('figure', dpi=100)
+plt.rc('legend', fontsize=18.0)
+plt.rc('xtick', labelsize=18.0)
+plt.rc('ytick', labelsize=18.0)
+plt.rc('figure', dpi=200)
 
 H1dat      = loadmat('Data/' + 'H1_SeismicBLRMS_March.mat')
 vdat       = H1dat['data'][-6:]
 vchans     = H1dat['chans'][-6:]
 timetuples = vdat.T
 
-cl = 6
+N_clusters = 6
 colors = np.array(['r', 'g', 'b', 'y', 'c', 'm'])
 
-kmeans = KMeans(n_clusters=cl, random_state=12).fit(timetuples)
+print("K-Means Clustering...")
+kmeans = KMeans(n_clusters=N_clusters, random_state=12).fit(timetuples)
 
-xvals = (np.arange(len(vdat[0])))/(60.*24.)
+xvals = (np.arange(len(vdat[0])))/(60*24)
 
-fig, axes = plt.subplots(len(vdat), figsize=(40,4*len(vdat)))
+print("Plotting...")
+fig, axes = plt.subplots(len(vdat), figsize=(40, 4*len(vdat)))
 for ax, data, chan in zip(axes, vdat, vchans):
-    ax.scatter(xvals, data, c=colors[kmeans.labels_], edgecolor=None, 
+    ax.scatter(xvals, data, c=colors[kmeans.labels_], edgecolor=None,
                s=3, label=r'$\mathrm{%s}$' % chan.replace('_','\_'))
     ax.set_yscale('log')
     ax.set_ylim(np.median(data)*0.1, max(data)*1.1)
@@ -42,10 +44,12 @@ for ax, data, chan in zip(axes, vdat, vchans):
     ax.grid(True, which='both')
     ax.legend()
 fig.tight_layout()
+print("Saving figs...")
+figname = 'SeismicBLRMS_Kmeans'
 try:
-    fig.savefig('Figures/' + 'sei-test3.png')
+    fig.savefig('Figures/' + figname)
 except RuntimeError as e:
     if 'latex' in str(e).lower():
-        fig.savefig('Figures/' + 'sei-test3.png')
+        fig.savefig('Figures/' + figname)
     else:
         raise

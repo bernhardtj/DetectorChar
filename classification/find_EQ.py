4f20f74af2438689e8aaaab47fedfc0e4ebbee88
Rana Adhikari <rana@caltech.edu>
2017-08-17 16:20:15 -0700

    made changes because code wouldn't run without "Roxana" directories

--- classification/find_EQ.py
+++ classification/find_EQ.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import numpy as np
 import scipy.io as sio
 from astropy.time import Time
@@ -8,31 +9,31 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
 from scipy import signal
 
-#read in data
+# read in data
 H1dat = sio.loadmat('Data/' + 'H1_SeismicBLRMS.mat')
-edat = np.loadtxt('Data/H1_earthquakes.txt')
+edat  = np.loadtxt('Data/H1_earthquakes.txt')
 
 #read in earthquake channels
-cols = [6,12,18,24,30,36,42,48]
-vdat = np.array(H1dat['data'][0])
+cols   = [6,12,18,24,30,36,42,48]
+vdat   = np.array(H1dat['data'][0])
 vchans = np.array(H1dat['chans'][0])
 for i in cols:
-    add = np.array(H1dat['data'][i])
-    vdat = np.vstack((vdat, add))
+    add    = np.array(H1dat['data'][i])
+    vdat   = np.vstack((vdat, add))
     vchans = np.append(vchans,H1dat['chans'][i])
 
 #convert time to gps time                      
-times = '2017-03-01 00:00:00'
-t = Time(times,format='iso',scale='utc')
-t_start= int(np.floor(t.gps/60)*60)
-dur_in_days= 30
+times   = '2017-03-01 00:00:00'
+t       = Time(times,format='iso',scale='utc')
+t_start = int(np.floor(t.gps/60)*60)
+dur_in_days = 30
 dur_in_minutes = dur_in_days*24*60
-dur = dur_in_minutes*60
-t_end = t_start+dur
-xvals = np.arange(t_start,t_end, 60)
+dur     = dur_in_minutes*60
+t_end   = t_start+dur
+xvals   = np.arange(t_start,t_end, 60)
 
-widths = np.arange(35,40)
-peaks = signal.find_peaks_cwt(vdat[0], widths)
+widths  = np.arange(35,40)
+peaks   = signal.find_peaks_cwt(vdat[0], widths)
 
 print(peaks)
 
@@ -53,4 +54,7 @@ for ax, data, chan in zip(axes, vdat, vchans):
     for e in range(len(EQ_locations)):
         ax.axvline(x=EQ_locations[e], color = 'r')
 fig.tight_layout()
-fig.savefig('/home/roxana.popescu/public_html/'+'EQ_peaks_indicated.png')
+fig.savefig('Figures/EQ_peaks_indicated.png')
+
+# can't have these hard coded path names; doesn't run for anyone else this way
+#fig.savefig('/home/roxana.popescu/public_html/'+'EQ_peaks_indicated.png')
